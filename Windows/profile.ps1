ea7395ffb30ca748342298e84fec99dfeb1b3206
Erik451 <47476901+erik-451@users.noreply.github.com>
2022-05-15 14:43:24 +0200

    Create profile.ps1

---
+++ Windows/profile.ps1
@@ -0,0 +1,17 @@
+# Shell
+Import-Module posh-git
+Import-Module oh-my-posh
+oh-my-posh.exe init pwsh -c 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/slimfat.omp.json'|Invoke-Expression
+Import-Module -Name Terminal-Icons
+# Buscar
+Import-Module PSFzf
+Set-PsFzfOption -PsReadLineChordProvider 'Ctrl+f' -PSReadLineChordReverseHistory 'Ctrl+r'
+# Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig  'D:\2-Programas\Git\Git\usr\bin\tig.exe'
+Set-Alias less  'D:\2-Programas\Git\Git\usr\bin\less.exe'
+# Clear the banner
+cls
