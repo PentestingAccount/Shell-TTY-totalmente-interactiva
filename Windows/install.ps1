14fbee845aaf14f2a3fe3cd4547634c29e3508a7
Erik451 <47476901+erik-451@users.noreply.github.com>
2022-05-15 15:07:01 +0200

    Update install.ps1

--- Windows/install.ps1
+++ Windows/install.ps1
@@ -3,20 +3,29 @@ iwr -useb get.scoop.sh | iex
 Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 scoop install curl sudo jq
 scoop install neovim gcc
+
 # Instalar posh-git
 Install-Module posh-git -Scope CurrentUser -Force
 Import-Module posh-git
+
 # Instalar oh-my-posh
 Install-Module oh-my-posh -Scope CurrentUser -Force
 Import-Module oh-my-posh
+
+# Instalar nerdfonts
+scoop bucket add nerd-fonts
+scoop install DejaVuSansMono-NF
+
 # Instalar iconos
 Install-Module -Name Terminal-Icons -Repository PSGallery -Force
 Import-Module Terminal-Icons
+
 ## Instalar moduleos de Autocompleatado
 Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
 Install-Module PSReadLine -Force
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
+
 # Instalar modulos de Buscar archivos/carpetas
 scoop install fzf
 Install-Module -Name PSFzf -Scope CurrentUser -Force
