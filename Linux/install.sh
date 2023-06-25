2c7eb8d7d0d6fc0e969f3e642f311ac9a7ee84f0
Erik451 <47476901+erik-451@users.noreply.github.com>
2023-06-25 16:36:31 +0200

    Update install.sh

--- Linux/install.sh
+++ Linux/install.sh
@@ -7,18 +7,19 @@
 #################
 # Descargando git zsh tmux curl
 echo "[*] Instalando dependencias"
-sudo apt install -y git zsh tmux curl unzip wget subversion
+sudo apt install -y git zsh tmux curl xclip wget 
+# sudo apt install -y subversion unzip
 
 #######################
 #     oh my posh      #
 # << DESHABILITADO >> #
 #######################
 # Descargando oh my posh
-echo "[*] Descargando oh-my-posh"
-wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
-chmod +x /usr/local/bin/oh-my-posh
-## Descargando temas de oh my posh
+# echo "[*] Descargando oh-my-posh"
+# wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+# chmod +x /usr/local/bin/oh-my-posh
 
+## Descargando temas de oh my posh
 # echo "[*] Descargando temas..."
 # mkdir ~/.poshthemes
 # wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
@@ -30,11 +31,7 @@ chmod +x /usr/local/bin/oh-my-posh
 ## temas: https://github.com/JanDeDobbeleer/oh-my-posh/tree/main/themes
 # echo 'eval "$(oh-my-posh init zsh --config ~/.poshthemes/slimfat.omp.json)"' >> ~/.zshrc
 
-# Se crea la carpeta cache si no esta creada
-until [ -d $HOME/.cache ];
-do
-	mkdir $HOME/.cache
-done
+
 
 #############
 # Oh my zsh #
@@ -43,7 +40,7 @@ done
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # zsh por predeterminada
-chsh -s zsh
+chsh -s /usr/bin/zsh
 
 ##########
 ## TMUX ##
@@ -53,7 +50,7 @@ echo "[*] Descargando entorno tmux"
 curl -s "https://raw.githubusercontent.com/erik-451/Custom-shells/main/Linux/.tmux.conf.md" > $HOME/.tmux.conf
 
 # Asignamos que la shell se inicie con tmux
-echo -e "if command -v tmux>/dev/null; then\n   [[ ! \$TERM =~ screen ]] && [ -z \$TMUX ] && exec tmux\nfi\n" >> .zshrc
+echo -e "if command -v tmux>/dev/null; then\n   [[ ! \$TERM =~ screen ]] && [ -z \$TMUX ] && exec tmux\nfi\n" >> $HOME/.zshrc
 
 echo "[*] Success!!!"
 
