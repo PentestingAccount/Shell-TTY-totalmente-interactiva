a9d4a6442e8247d67c9931d7f24e8aae85dc4b67
Erik451 <47476901+erik-451@users.noreply.github.com>
2023-06-25 16:38:25 +0200

    Update install.sh

--- Android-Termux/install.sh
+++ Android-Termux/install.sh
@@ -11,31 +11,27 @@ echo "extra-keys = [['ESC','CTRL','ALT','HOME','UP','END','PGUP'], \ \n         
 #################
 # Descargando git zsh tmux curl
 echo "[*] Instalando dependencias"
-apt install -y git zsh tmux curl unzip wget subversion
+apt install -y git zsh tmux curl xclip wget
 ##############
 # oh my posh #
 ##############
 # Descargando oh my posh
-echo "[*] Descargando oh-my-posh"
-wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-arm64 -O $HOME/../usr/bin/oh-my-posh
-chmod +x $HOME/../usr/bin/oh-my-posh
+#echo "[*] Descargando oh-my-posh"
+#wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-arm64 -O $HOME/../usr/bin/oh-my-posh
+#chmod +x $HOME/../usr/bin/oh-my-posh
 
 # Descargando temas de oh my posh
-echo "[*] Descargando temas..."
-mkdir ~/.poshthemes
-wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
-unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
-chmod u+rw ~/.poshthemes/*.json
-rm ~/.poshthemes/themes.zip
+#echo "[*] Descargando temas..."
+#mkdir ~/.poshthemes
+#wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
+#unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
+#chmod u+rw ~/.poshthemes/*.json
+#rm ~/.poshthemes/themes.zip
 
 # zsh por predeterminada
 chsh -s zsh
 
-# Se crea la carpeta cache si no esta creada
-until [ -d $HOME/.cache ];
-do
-	mkdir $HOME/.cache
-done
+
 #############
 # Oh my zsh #
 #############
@@ -44,7 +40,7 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 # Aplicando tema a la shell || puedes sustituir el tema por el que mas te guste
 # temas: https://github.com/JanDeDobbeleer/oh-my-posh/tree/main/themes
-echo 'eval "$(oh-my-posh init zsh --config ~/.poshthemes/slimfat.omp.json)"' >> ~/.zshrc
+echo 'eval "$(oh-my-posh init zsh --config ~/.poshthemes/slimfat.omp.json)"' >> $HOME/.zshrc
 
 ##########
 ## TMUX ##
@@ -52,12 +48,9 @@ echo 'eval "$(oh-my-posh init zsh --config ~/.poshthemes/slimfat.omp.json)"' >> 
 # Usando el tema de tmux personalizado
 echo "[*] Descargando entorno tmux"
 curl -s "https://raw.githubusercontent.com/erik-451/Custom-shells/main/Android-Termux/.tmux.conf.md" > $HOME/.tmux.conf
-# Asignamos que la shell se inicie con tmux
-if [[ ! $(tmux list-sessions) ]]; then
-  tmux
-fi
-if [ -z $TMUX ]; then; tmux; fi
 
+# Asignamos que la shell se inicie con tmux
+echo -e "if command -v tmux>/dev/null; then\n   [[ ! \$TERM =~ screen ]] && [ -z \$TMUX ] && exec tmux\nfi\n" >> $HOME/.zshrc
 echo "[*] Success!!!"
 
 
